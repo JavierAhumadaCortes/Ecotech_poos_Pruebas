74b8c9136274653762d54c7d6e807633714412be
Laboratorio Computación N° 7 <claseslab7@inacap.cl>
2026-09-10 12:03:09 -0300

    clase 2

--- src/dominio/empleado.py
+++ src/dominio/empleado.py
@@ -1,8 +1,18 @@
 # src/dominio/empleado.py​
+from dominio.registroTiempo import RegistroTiempo
+
 class Empleado:
     def __init__(self, nombre: str, correo: str):
         self.nombre = nombre
         self.correo = correo
+        self._registro_tiempo = list[RegistroTiempo] = []
 
     def mostrar_datos(self) -> str:
         return f"{self.nombre} - {self.correo}"
+
+    def registrar_tiempo(self, registroTiempo: RegistroTiempo) -> bool:
+        if registroTiempo in self._registro_tiempo:
+            return False
+
+        self._registro_tiempo.append(registroTiempo)
+        return True
--- src/main.py
+++ src/main.py
@@ -1,9 +1,34 @@
 # src/main.py​
 from dominio.empleado import Empleado
+from dominio.departamento import Departamento
+from dominio.departamento import RegistroTiempo
 
 empleado = Empleado(
     nombre="Ana Torres",
     correo="ana.torres@ecotech.cl"
 )
+empleado2 = Empleado(
+    nombre="Ana3 Torres",
+    correo="ana.torres@ecotech.cl"
+)
+empleado3 = Empleado(
+    nombre="Ana4 Torres",
+    correo="ana.torres@ecotech.cl"
+)
 
-print(empleado.mostrar_datos())
+empleado3.registrar_tiempo(RegistroTiempo('10/12/2026','10:50'))
+empleado3.registrar_tiempo(RegistroTiempo('11/12/2026','10:50'))
+rgs1 = RegistroTiempo('12/12/2026','10:50')
+empleado3.registrar_tiempo(rgs1)
+
+#print(empleado.mostrar_datos())
+desarrollo = Departamento("DEp Desarrollo")
+desarrollo.agregar_empleado(empleado)
+desarrollo.agregar_empleado(empleado2)
+desarrollo.agregar_empleado(empleado3)
+
+
+print(desarrollo.cantidad_empleados())
+
+for empleado in desarrollo.empleados:
+    print(empleado.mostrar_datos())
