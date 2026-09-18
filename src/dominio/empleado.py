ae5b194402211673655fe1996efe50a99bec83cc
Javier Ahumada <javier.ahumada@inacapmail.cl>
2026-09-18 18:01:05 -0300

    clase 4

--- src/dominio/empleado.py
+++ src/dominio/empleado.py
@@ -2,17 +2,10 @@
 from dominio.registroTiempo import RegistroTiempo
 
 class Empleado:
-    def __init__(self, nombre: str, correo: str):
+    def __init__(self, nombre, correo, id=None):
+        self.id = id
         self.nombre = nombre
         self.correo = correo
-        self._registro_tiempo = list[RegistroTiempo] = []
 
     def mostrar_datos(self) -> str:
         return f"{self.nombre} - {self.correo}"
-
-    def registrar_tiempo(self, registroTiempo: RegistroTiempo) -> bool:
-        if registroTiempo in self._registro_tiempo:
-            return False
-
-        self._registro_tiempo.append(registroTiempo)
-        return True
--- src/main.py
+++ src/main.py
@@ -1,34 +1,15 @@
-# src/main.py​
+# main.py
+from persistencia.crear_bd import crear_tablas
 from dominio.empleado import Empleado
-from dominio.departamento import Departamento
-from dominio.departamento import RegistroTiempo
+from persistencia.empleado_dao import EmpleadoDAO
 
-empleado = Empleado(
-    nombre="Ana Torres",
-    correo="ana.torres@ecotech.cl"
-)
-empleado2 = Empleado(
-    nombre="Ana3 Torres",
-    correo="ana.torres@ecotech.cl"
-)
-empleado3 = Empleado(
-    nombre="Ana4 Torres",
-    correo="ana.torres@ecotech.cl"
-)
+crear_tablas()
+empleado = Empleado(nombre="Ana Pérez", correo="ana@ecotech.cl")
 
-empleado3.registrar_tiempo(RegistroTiempo('10/12/2026','10:50'))
-empleado3.registrar_tiempo(RegistroTiempo('11/12/2026','10:50'))
-rgs1 = RegistroTiempo('12/12/2026','10:50')
-empleado3.registrar_tiempo(rgs1)
+print("Antes:", empleado.id)
+# None
 
-#print(empleado.mostrar_datos())
-desarrollo = Departamento("DEp Desarrollo")
-desarrollo.agregar_empleado(empleado)
-desarrollo.agregar_empleado(empleado2)
-desarrollo.agregar_empleado(empleado3)
+EmpleadoDAO.insertar(empleado)
 
-
-print(desarrollo.cantidad_empleados())
-
-for empleado in desarrollo.empleados:
-    print(empleado.mostrar_datos())
+print("Después:", empleado.id)
+# id generado por la BD
